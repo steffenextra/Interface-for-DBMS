ebb9552848bed57028a0d5e1814f43d7ebd031c6
steffenextra <steffen.extra@web.de>
2018-11-05 12:04:38 +0100

    shellscript fix for ubuntu

--- prototyp/comp.sh
+++ prototyp/comp.sh
@@ -4,6 +4,11 @@
 	then
 			clang++ -std=c++11 -I/usr/local/mysql/include -L/usr/local/opt/openssl/lib -I/usr/local/opt/openssl/include -L/usr/local/mysql/lib -lmysqlclient -lssl -lcrypto  *.cpp *.hpp
 			rm -rf *.hpp.gch
+
+	elif [ -d /usr/include/openssl ];
+	then
+			clang++ -std=c++11  -I -L/usr/include/openssl  -I -L /usr/include/mysql  -lmysqlclient -lssl -lcrypto *.cpp *.hpp
+
 	fi
 
 
